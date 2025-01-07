f5af0dc3e608539dd5789be490ea3815d2b86506
maxsld <soldan.maxens@gmail.com>
2025-01-07 16:35:16 +0100

    `Added podium ranking system to profile page`

--- app/assets/js/profile_script.js
+++ app/assets/js/profile_script.js
@@ -187,3 +187,51 @@ fetch("http://localhost:5000/api/getUserInfo", {
   .catch((error) => {
     console.error("Error fetching user info:", error);
   });
+
+
+  document.getElementById('link_rang').addEventListener('click', function (event) {
+    event.preventDefault(); // Empêcher la redirection par défaut
+    localStorage.setItem('activeTab', 'rang-tab'); // Stocker l'ID de l'onglet à activer
+    window.location.href = 'profile.html'; // Rediriger vers profile.html ou la page que vous souhaitez
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".profile__tab");
+    const contents = document.querySelectorAll(".profile__tabs-content > div");
+  
+    // Fonction pour changer d'onglet
+    function changeTab(event) {
+      const target = event.target;
+  
+      tabs.forEach((tab) => tab.classList.remove("active"));
+      contents.forEach((content) => content.classList.remove("active"));
+  
+      target.classList.add("active");
+      const index = Array.from(tabs).indexOf(target);
+      contents[index].classList.add("active");
+    }
+  
+    // Activer l'onglet spécifié dans localStorage
+    const activeTabId = localStorage.getItem("activeTab");
+    if (activeTabId) {
+      const activeTab = document.getElementById(activeTabId);
+      if (activeTab) {
+        // Activer l'onglet
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        contents.forEach((content) => content.classList.remove("active"));
+  
+        activeTab.classList.add("active");
+        const index = Array.from(tabs).indexOf(activeTab);
+        if (index >= 0) {
+          contents[index].classList.add("active");
+        }
+      }
+      localStorage.removeItem("activeTab"); // Nettoyer le stockage après utilisation
+    }
+  
+    // Ajouter des écouteurs pour les clics sur les onglets
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", changeTab);
+    });
+  });
+  
